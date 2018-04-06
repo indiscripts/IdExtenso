3afd39d5641982364e74e9669d768a8f38eba813
Marc Autret <marc@indiscripts.com>
2018-04-06 20:15:19 +0200

    ShowHelp - Discover IdExtenso's API (Upd.) [180406]

--- tests/ShowHelp.jsx
+++ tests/ShowHelp.jsx
@@ -8,18 +8,19 @@
 
 // Including some extra modules.
 // ---
-#include '../etc/$$.Yalt.jsxlib'
 #include '../etc/$$.Web.jsxlib'
 #include '../etc/$$.BigInt.jsxlib'
 #include '../etc/$$.Random.jsxlib'
-#include '../etc/$$.Settings.jsxlib'
+#include '../etc/$$.BasicScript.jsxlib'
+#include '../etc/$$.Complex.jsxlib'
+#include '../etc/$$.DateFormat.jsxlib'
 
 // Load the framework.
 // ---
 $$.load();
 
 // =============================================================================
-// ShowHelp [171111]
+// ShowHelp [171111] [180406]
 // Get help on core and included modules.
 // ---
 // Demonstrates:
