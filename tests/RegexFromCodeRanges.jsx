1541f66e7ca7a9591e5ed3307139579c8ea0f9c4
Marc Autret <marc@indiscripts.com>
2020-10-05 16:27:00 +0200

    RegexFromCodeRanges - Improved [201005]

--- tests/RegexFromCodeRanges.jsx
+++ tests/RegexFromCodeRanges.jsx
@@ -11,24 +11,26 @@
 $$.load(0);
 
 // =============================================================================
-// RegexFromCodeRanges [200923]
+// RegexFromCodeRanges [200923] [201005]
 // -----------------
 // Take advantage of `RegExp.fromCodeRanges`.
 // Demonstrates:
 // - Converting a set of code ranges into a RegExp pattern.
-// - Limitations (surrogate pairs must be consistent, i.e share the same base.)
 // - Producing a regex that captures misc. symbols and pictographs (1F300-1F5FF)
 // =============================================================================
 try
 {
+	alert( RegExp.fromCodeRanges('17000..187FF') );
+	
+	
 	var tests =
 	{
-		'Basic range supplied as string':     "41..7F",
-		'Array using various formats':        [ 0x9, '0041..005A', [0xC0,0xD6], ['D8','DE'], '0100', 0x102 ],
-		'Including surrogate pairs (> FFFF)': [ '1F5F', '1F68..1F6F', ['FF21','FF3A'], '10400..10427' ],
-		'Surrogate pairs, various forms':     [ 0x1D4BB,[0x1D4C5,'1D4CF'],'1D7AA..1D7C2',['1E922',0x1E943] ],
-		'Including huge range (> 100000)':    [ '1D4D0..1D4FF', [0x10FF00, 0x10FFFF] ],
-		'Error with inconsistent range':      "3134B..E0000", // These don't share the same base
+		'1. Basic range supplied as string':        "41..7F",
+		'2. Array using various formats':           [ 0x9, '0041..005A', [0xC0,0xD6], ['D8','DE'], '0100', 0x102 ],
+		'3. Including surrogate pairs (> FFFF)':    [ '1F5F', '1F68..1F6F', ['FF21','FF3A'], '10400..10427' ],
+		'4. Surrogate pairs, various forms':        [ 0x1D4BB,[0x1D4C5,'1D4CF'],'1D7AA..1D7C2',['1E922',0x1E943] ],
+		'5. Including huge codepoints (> 100000)':  [ '1D4D0..1D4FF', [0x10FF00, 0x10FFFF] ],
+		'6. Smart support of large ranges (NEW!)':  "1F5F4..20AD2", // [CHG200924]
 	};
 
 	var k,input,output,msg;
@@ -45,19 +47,19 @@ try
 			, output
 		));
 
-		// => [\u0041-\u007F]
-		// => [\u0009\u0041-\u005A\u00C0-\u00D6\u00D8-\u00DE\u0100\u0102]
-		// => [\u1F5F\u1F68-\u1F6F\uFF21-\uFF3A]|\uD801[\uDC00-\uDC27]
-		// => \uD835\uDCBB|\uD835[\uDCC5-\uDCCF]|\uD835[\uDFAA-\uDFC2]|\uD83A[\uDD22-\uDD43]
-		// => \uD835[\uDCD0-\uDCFF]|\uDBFF[\uDF00-\uDFFF]
-		// => Error: Code point [U+E0000 =\uDB40\uDC00] is out of range
+		// 1 => [\u0041-\u007F]
+		// 2 => [\u0009\u0041-\u005A\u00C0-\u00D6\u00D8-\u00DE\u0100\u0102]
+		// 3 => [\u1F5F\u1F68-\u1F6F\uFF21-\uFF3A]|\uD801[\uDC00-\uDC27]
+		// 4 => \uD835\uDCBB|\uD835[\uDCC5-\uDCCF]|\uD835[\uDFAA-\uDFC2]|\uD83A[\uDD22-\uDD43]
+		// 5 => \uD835[\uDCD0-\uDCFF]|\uDBFF[\uDF00-\uDFFF]
+		// 6 => \uD83D[\uDDF4-\uDFFF]|[\uD83E-\uD841][\uDC00-\uDFFF]|\uD842[\uDC00-\uDED2]
 	}
 	
 
-	// We cannot send the range `1F300..1F5FF` in one piece but the
-	// below code is OK. The regex will capture symbols and pictos.
+	// [CHG200924] We now can send the range `1F300..1F5FF` in one piece and the
+	// resulting regex is compact: /\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF]/
 	// ---
-	const RE_PICTOS = RegExp.fromCodeRanges( ['1F300..1F3FF', '1F400..1F4FF','1F500..1F5FF'], true);
+	const RE_PICTOS = RegExp.fromCodeRanges( '1F300..1F5FF', true);
 
 	// Note: in case your string would come from UTF8 encoding, use `String.fromUTF8()` instead.
 	// ---
